cd0570fc08dd8a30a4509c6d552355dff3838e29
femia <femi.adesiyan@student.kuleuven.be>
2024-10-23 10:06:52 +0200

    Acceleration added to the player movement. Also maximum speed

--- testinggit/Assets/PlayerMovement.cs
+++ testinggit/Assets/PlayerMovement.cs
@@ -5,39 +5,59 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    public float movingSpeed = 500f;
+    //public float movingSpeed = 500f;
     public float jumpForce = 500f;
+    public float maxSpeed = 2000f;
+    public float acceleration = 50f;
+
+    [SerializeField]private Vector3 currentSpeed; //for the players curruent speed
 
     // Start is called before the first frame update
     void Start()
     {
         //rb.useGravity = false;
         //rb.AddForce(0,200,500);
+        currentSpeed= Vector3.zero;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         //rb.AddForce (0,0,forwardForce * Time.deltaTime);
+        bool isMoving = false;
 
         if ( Input.GetKey(KeyCode.RightArrow))
         {
-            rb.AddForce(movingSpeed * Time.deltaTime, 0, 0);
+            //rb.AddForce(movingSpeed * Time.deltaTime, 0, 0);
+            Accelerate(Vector3.right);
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.AddForce(-movingSpeed * Time.deltaTime, 0, 0);
+            //rb.AddForce(-movingSpeed * Time.deltaTime, 0, 0);
+            Accelerate(Vector3.left);
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rb.AddForce(0, 0, movingSpeed * Time.deltaTime);
+            //rb.AddForce(0, 0, movingSpeed * Time.deltaTime);
+            Accelerate(Vector3.forward);
+            isMoving = true;
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            rb.AddForce(0, 0, -movingSpeed * Time.deltaTime);
+            //rb.AddForce(0, 0, -movingSpeed * Time.deltaTime);
+            Accelerate(Vector3.back);
+            isMoving = true;
+        }
+
+        // Stop movement when no keys are pressed
+        if (!isMoving)
+        {
+            ResetVelocity();
         }
 
         if (Input.GetKey(KeyCode.Space))
@@ -45,4 +65,18 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(0, jumpForce * Time.deltaTime, 0);
         }
     }
+
+
+    public void Accelerate (Vector3 direction)
+    {
+        currentSpeed += direction * acceleration * Time.deltaTime;
+        currentSpeed = Vector3.ClampMagnitude(currentSpeed, maxSpeed);
+        rb.AddForce(currentSpeed *  Time.deltaTime);
+    }
+
+    public void ResetVelocity()
+    {
+        currentSpeed = Vector3.zero;
+    }
 }
+    
